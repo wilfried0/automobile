fd9e7d5003d57bf903840e74b6f930bf5849bf1a
wilfried0 <negprodattitude@gmail.com>
2023-04-19 11:08:12 +0100

    initialisation du projet

---
+++ src/test/java/com/tutorial/automobile/AutomobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.automobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
